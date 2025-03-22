0e834165d97a4e9fe7e83519c5a51b4fcc326466
Sant0s0 <SantiWorldYT@gmail.com>
2025-03-22 16:49:04 -0500

    agrego cambios

--- Tienda.h
+++ Tienda.h
@@ -27,18 +27,7 @@ public:
     void listarProductos() {
         for (size_t i = 0; i < productos.size(); i++) {
             productos[i].mostrarinformacion();
-            cout << "Índice: " << i << endl;
         }
     }
 };
-int main(){
-  Tienda tienda;
-  string cod = "1234";
-  string nom = "arroz";
-  int cantidad = 2;
-  int costo = 2000;
-  Producto arroz(cod, nom, costo, cantidad);
-  tienda.agregarProducto(cod, nom, costo, cantidad);
-}
-
 #endif //TIENDA_H
--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-/*// #include <iostream>
-// #include "Producto.h"
-//
-// using namespace std;
-// int main(){
-//     Tienda tienda;
-//     string cod = "1234";
-//     string nom = "arroz";
-//     int cantidad = 2;
-//     int costo = 2000;
-//     Producto arroz(cod, nom, costo, cantidad);
-//     tienda.agregarProducto(cod, nom, costo, cantidad);
-// }*/
-
-//esto aun no funciona :pensive:
+#include <iostream>
+#include "Producto.h"
+#include "Tienda.h"
+using namespace std;
+int main(){
+    Tienda tienda;
+    string cod = "1234";
+    string nom = "arroz";
+    int cantidad = 2;
+    int costo = 2000;
+    Producto arroz(cod, nom, costo, cantidad);
+    tienda.agregarProducto(cod, nom, costo, cantidad);
+    tienda.listarProductos();
+    return 0;
+}
